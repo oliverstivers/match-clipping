1d704e4f67be01c5fe4acdd5e84be670db03931b
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2024-02-11 11:21:01 -0800

    added start time to extracting audio

--- clip_sound_from_video.py
+++ clip_sound_from_video.py
@@ -1,6 +1,16 @@
 import subprocess
 
-def extract_audio(input_file, output_file):
-    command = ["ffmpeg", "-i", input_file, "-vn", "-acodec", "copy", output_file]
-    subprocess.run(command, check=True)
 
+def extract_audio(input_file, output_file, start_seconds=0):
+    command = [
+        "ffmpeg",
+        "-i",
+        input_file,
+        "-ss",
+        str(start_seconds),
+        "-vn",
+        "-acodec",
+        "copy",
+        output_file,
+    ]
+    subprocess.run(command, check=True)
